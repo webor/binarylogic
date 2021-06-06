7a0b51407e42336db8d3920050a501f1b2fddf40
webor <tambatvibhor@gmail.com>
2021-06-06 09:24:15 +0530

    Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img id="logo" alt="Binary Logic Logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Binary Logic . A Tech Blogging Portal" />
   </div>
 </template>
-
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Options({
   components: {
@@ -16,3 +15,9 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+<style scoped lang="scss">
+#logo {
+  width: 100px;
+  height: 100px;
+}
+</style>
