8ccf076f0407a974dda9873b51deb671f2e0f334
Vibhor Tambat <kli.vibhor-tambat@kotak.com>
2024-02-23 01:08:31 +0530

    Add New blog page links dist files

---
+++ dist/precache-manifest.ca0281d01d03766289833f9349a3bfa9.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce02d809d445a287702",
+    "url": "/css/about.906495c5.css"
+  },
+  {
+    "revision": "b5ef43760c87282d9489",
+    "url": "/css/app.728d7d67.css"
+  },
+  {
+    "revision": "e7eeb252b657f3c91b8e",
+    "url": "/css/chunk-vendors.544aa21f.css"
+  },
+  {
+    "revision": "486a01703949b481a363fbe673910e37",
+    "url": "/favicon_black.ico"
+  },
+  {
+    "revision": "1cfa76e9fb5a0a76d7baf6bfa4be3c34",
+    "url": "/img/Octocat.1cfa76e9.png"
+  },
+  {
+    "revision": "08274661ef51743e167e9d9039e97052",
+    "url": "/img/android.08274661.png"
+  },
+  {
+    "revision": "2fb0a23ae38ff99569c176bd96191629",
+    "url": "/img/angular.2fb0a23a.png"
+  },
+  {
+    "revision": "4bf9351da03c7b4359f2c52d2b9dc9de",
+    "url": "/img/apache.4bf9351d.png"
+  },
+  {
+    "revision": "dcf4eb25e8b7cc503296f7b6aa752096",
+    "url": "/img/aws.dcf4eb25.png"
+  },
+  {
+    "revision": "95d6f7398423af900efff6d67156eeb4",
+    "url": "/img/bash.95d6f739.png"
+  },
+  {
+    "revision": "687d57a95811bbb5b72fd81b7b3d3857",
+    "url": "/img/cert_1.687d57a9.jpg"
+  },
+  {
+    "revision": "14c91f8f5ee636c60f0f6c35f11ee53e",
+    "url": "/img/css.14c91f8f.png"
+  },
+  {
+    "revision": "d79e6e3224396d4b250711be6c927c40",
+    "url": "/img/expo.d79e6e32.png"
+  },
+  {
+    "revision": "e812bec137af36c10dfe4b944efa000b",
+    "url": "/img/express.e812bec1.png"
+  },
+  {
+    "revision": "07f53ce2b532318294da377cdb3ee798",
+    "url": "/img/fastify.07f53ce2.png"
+  },
+  {
+    "revision": "52aa91c0094a0fa8f957d10900532c87",
+    "url": "/img/firebase.52aa91c0.png"
+  },
+  {
+    "revision": "5b448728157a6d1d08228bf3343496a7",
+    "url": "/img/go.5b448728.png"
+  },
+  {
+    "revision": "4228fc53e429afb836ef6e997ceab43f",
+    "url": "/img/gs.4228fc53.png"
+  },
+  {
+    "revision": "f374d036d2fb8b23ac7635316b3cdd08",
+    "url": "/img/html.f374d036.png"
+  },
+  {
+    "revision": "83dbd4d58df64e9545fcdfbae0acb7ea",
+    "url": "/img/ionic.83dbd4d5.png"
+  },
+  {
+    "revision": "c85f9173b8134e5a5348cd1c684d93ed",
+    "url": "/img/ios.c85f9173.png"
+  },
+  {
+    "revision": "80d0ac01dcd50c7bbc89eab2ae9adc07",
+    "url": "/img/java.80d0ac01.png"
+  },
+  {
+    "revision": "bb04496cbeafb34f0d44dcce8f6e1631",
+    "url": "/img/jenkins.bb04496c.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/img/js.0cded3a3.png"
+  },
+  {
+    "revision": "5a200a70d248663b1afaca2c96502ad6",
+    "url": "/img/json.5a200a70.png"
+  },
+  {
+    "revision": "00f3a19f9b1f94ef13e5916fbac22a09",
+    "url": "/img/kubernetes.00f3a19f.png"
+  },
+  {
+    "revision": "402d648d96614eb2a62ac846a74f6134",
+    "url": "/img/logo.402d648d.png"
+  },
+  {
+    "revision": "60605a1a458a3de31e7e4b5cc3605d13",
+    "url": "/img/mongo.60605a1a.png"
+  },
+  {
+    "revision": "270f55fffea36219b66a63fbe03ff497",
+    "url": "/img/node.270f55ff.png"
+  },
+  {
+    "revision": "0433e79e34d5000ecfc24ff4d53488bb",
+    "url": "/img/photo.0433e79e.jpeg"
+  },
+  {
+    "revision": "743108e230805b40a7795bf3aa14a250",
+    "url": "/img/project_1.743108e2.png"
+  },
+  {
+    "revision": "af981df21f2ecb748e980804f0f1ba17",
+    "url": "/img/project_2.af981df2.png"
+  },
+  {
+    "revision": "75deaa27e9843b1e263d97632539fd79",
+    "url": "/img/project_3.75deaa27.png"
+  },
+  {
+    "revision": "cb86191526a598354e15f09be0d7229b",
+    "url": "/img/project_4.cb861915.png"
+  },
+  {
+    "revision": "1ce7f5497e3e51342de4546ffffbb57a",
+    "url": "/img/project_5.1ce7f549.png"
+  },
+  {
+    "revision": "bfcb6ac93510a752357c934f716610f3",
+    "url": "/img/project_6.bfcb6ac9.png"
+  },
+  {
+    "revision": "c28a1b91c12a2c7ab5ee61c528224a00",
+    "url": "/img/pwa.c28a1b91.png"
+  },
+  {
+    "revision": "9dacd616c5976c16a4c9ff4bfa959c9b",
+    "url": "/img/python.9dacd616.png"
+  },
+  {
+    "revision": "abfd04efe6ab7dfdb6a5b531eaafe0a5",
+    "url": "/img/react-native.abfd04ef.jpeg"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "/img/react.638be909.png"
+  },
+  {
+    "revision": "718c4b6386a59603b62b6894e310c411",
+    "url": "/img/redux.718c4b63.png"
+  },
+  {
+    "revision": "29708abea20c942666f33387b874d302",
+    "url": "/img/rxjs.29708abe.png"
+  },
+  {
+    "revision": "8656e839fa33def4d00520fc84194a98",
+    "url": "/img/ts.8656e839.png"
+  },
+  {
+    "revision": "afab989b494c04e1f5fd518b84b95ac7",
+    "url": "/img/vuejs.afab989b.png"
+  },
+  {
+    "revision": "c500a3801d8356a86da86a06c3d13a4d",
+    "url": "/img/webpack.c500a380.png"
+  },
+  {
+    "revision": "8b40b57e21ea334e9ac37d5acd9feedb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ce02d809d445a287702",
+    "url": "/js/about.992bde52.js"
+  },
+  {
+    "revision": "b5ef43760c87282d9489",
+    "url": "/js/app.2bac8fd8.js"
+  },
+  {
+    "revision": "e7eeb252b657f3c91b8e",
+    "url": "/js/chunk-vendors.00c91947.js"
+  },
+  {
+    "revision": "f6eeff42ad80f268b6ae2e106217c02d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "f9eb17b71e5de6d7baf6754adbbb860f",
+    "url": "/pdfs/resume.pdf"
+  },
+  {
+    "revision": "0433e79e34d5000ecfc24ff4d53488bb",
+    "url": "/photo.jpeg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b815e24131f3205fe8717d0e3b3f4fa2",
+    "url": "/wallpaper.png"
+  }
+]);
